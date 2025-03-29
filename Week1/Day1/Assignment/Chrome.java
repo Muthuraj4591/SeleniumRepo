b6e81dcf85a17e8d8037151eadc9cbbc416bc10f
Muthuraj4591 <89079940+Muthuraj4591@users.noreply.github.com>
2025-03-29 18:54:50 +0530

    Add files via upload
    Week1

---
+++ Week1/Day1/Assignment/Chrome.java
@@ -0,0 +1,30 @@
+package Week1.Day1.Assignment;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//version of type float with a value of 91.0f
+		float version = 91.0f;
+		
+		//developer of type Stringwith a value of "Google"
+		String developer = "Google";
+		
+		//isBeta of type booleanwith a value of false
+		boolean isBeta = false;
+		
+		//releaseYear of type int with a value of 2008
+		int releaseYear = 2008;
+				
+		//shortcutKey of type charwith a value of 'C'
+		char shortcutKey = 'C';
+		
+		System.out.println("Version is: " +version);
+		System.out.println("Developer is: " +developer);
+		System.out.println("IsBeta is: " +isBeta);
+		System.out.println("Release Year is: " +releaseYear);
+		System.out.println("Shortcut Key is: " +shortcutKey);
+	}
+
+}
---
+++ Week1/Day1/LearnDataTypes.java
@@ -0,0 +1,13 @@
+package Week1.Day1;
+
+public class LearnDataTypes {
+
+	public static void main(String[] args) {
+		//Variable
+		
+		byte age=30;
+		System.out.println("The Age Is:"+age);
+
+	}
+
+}
